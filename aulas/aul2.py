ccd38d7158a59345925ee4c56330d09c14df244f
Joao Pedro <jaauhp@gmail.com>
2024-02-24 00:26:41 -0300

    commit 2

---
+++ aulas/aul2.py
@@ -0,0 +1,5 @@
+num1 = int (input ('Digite um Numero'))
+num2 = int (input ('Digite outro Numero'))
+s = num1+num2
+
+print('A Soma entre',num1,'e',num2,'e de ', s)
--- exercicios/ex01.py
+++ exercicios/ex01.py
@@ -2,8 +2,11 @@
 
 #print('Ola,',nome,'Bem Vindo')
 
-num1 = input ('Digite um Numero')
-num2 = input ('Digite outro Numero')
+num1 = int (input ('Digite um Numero'))
+num2 = int (input ('Digite outro Numero'))
+s = num1+num2
+
+print('A Soma entre',num1,'e',num2,'e de ', s)
 
 
 
