b0e02966e94009e60e8a6500c3a4169507b399d9
Joao Pedro <jaauhp@gmail.com>
2024-02-25 19:54:40 -0300

    Update aul7.py

--- aulas/auala 7 op aritimeticos/aul7.py
+++ aulas/auala 7 op aritimeticos/aul7.py
@@ -1,4 +1,4 @@
-# peradores aritiméticos 
+# operadores aritiméticos 
 
 # + Adição
 # - Subtração
@@ -37,7 +37,7 @@ d = n1 / n2
 di = n1 // n2
 p = n1 ** n2
 
-print ('>>>A soma é {}, o produto é {}, e divisão {:2f}'.format(s,m,d) , end=' ')
+print ('A soma é {}, o produto é {}, e divisão {:.2f}'.format(s,m,d) , end=' ')
 print ('A divisão inteira {}, e a potência {}'.format(di,p))
 
 # :3f serve para controlar o numero de descimais que aparecem apos o ponto ex: 1.3333333 ou 1.333
