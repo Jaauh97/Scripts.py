9825631a91d1e47a9f1e5abe1f16512ccd42f239
Joao Pedro <jaauhp@gmail.com>
2024-02-26 11:18:10 -0300

    Up 01 26/02

--- exercicios/ex05/ex05.py
+++ exercicios/ex05/ex05.py
@@ -1,1 +1,6 @@
+r = int(input('Olá , Digite um número e veja seu antecessor e seu sucessor : '))
 
+r1 = r-1
+r2= r+1
+
+print('Hum , Você digitou o número {}. Seu antecessor é {} e seu sucessor é {}!! '.format(r,r1,r2))
---
+++ exercicios/ex06/ex06.py
@@ -0,0 +1,6 @@
+r = int(input('Olá , Digite um numero : '))
+r1 = r*2
+r2 = r*3
+r3 = r*r
+
+print('Hum , Você digitou {}. \nEsse número x2 é igual a {} , esse numero x3 é igual a {} . Lembrando que a raiz quadrada de {} é igual a {}.'.format(r,r1,r2,r,r3))
