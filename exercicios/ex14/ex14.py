0802b78fa2ef2861a56dfce8c2b6ed2c1550beef
Joao Pedro <jaauhp@gmail.com>
2024-02-26 21:25:05 -0300

    up 03 26/02

---
+++ exercicios/ex14/ex14.py
@@ -0,0 +1,4 @@
+print('-'*45)
+print('Digite a temperatura atual em graus celsius (ºC ) para converter em Fahrenheit (°F): ')
+g = float(input('>>> Temperatura: '))
+print('A temperatura de {}ºC corresponde a {}°F'.format(g, (g*1.8+32)))
