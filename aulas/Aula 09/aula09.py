85eda28b2356a276637a9fed59c79cde7fb2c642
Joao Pedro <jaauhp@gmail.com>
2024-02-29 11:32:43 -0300

    update 01 29/02

---
+++ aulas/Aula 09/aula09.py
@@ -0,0 +1,18 @@
+frase = 'Cursos em Video Python'
+dividido = frase.split()
+
+
+
+
+#print(dividido[2][3])
+#print(dividido[0])
+
+
+
+#print(frase.split())
+#print(frase.lower().find('video'))
+#print(frase.find('Video'))
+#print('Curso' in frase)
+#print(frase[2:9:2])
+#print(frase.replace('Python','Joao'))
+#frase = frase.replace('Python','Joao')
