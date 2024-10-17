8d1dc1572d7cc084bea6f7775f3bcbfd838b1448
João Pedro <jaauhp@gmail.com>
2024-10-17 09:42:48 -0300

    update 04 17.10

--- exercicios/ex44/ex44.py
+++ exercicios/ex44/ex44.py
@@ -1,12 +1,14 @@
 print('{:=^40}'.format(' Loja do Jaauh ')) #:=^40 é um estilo de formatação de que deixa o texto no centro
 
-preco = float(input('Qual o valor das compras: ')) #o usuário informa qual o valor da comprar
 
+preco = float(input('Qual o valor das compras: ')) #o usuário informa qual o valor da comprar
 print('''Formas de pagamento: 
 [ 1 ] á vista 
 [ 2 ] à vista cartão
 [ 3 ] 2x no cartão
 [ 4 ] 3x ou mais no cartão ''') #Menu de opções
+
+
 
 opcao = int(input('Qual a opção de pagamento: ')) # O usuário informa qual a opção de pagamento
 
@@ -25,7 +27,12 @@ elif opcao == 4:
     total = preco + (preco * 20 / 100) #total da compra recebendo mais %20 de juros
     totalparc = int(input('Quantas parcelas: ')) #perguntando ao usuário quantyas vezes ele vai parcelar
     parcela =  total / totalparc #O valor da parcela recebe o total do valor do produto , dividido pela quantidade de parcelas
-    print('Sua compra será parcelada em {}x de R${:.2f}'.format(totalparc , parcela)) 
+    print('Sua compra será parcelada em {}x de R${:.2f}'.format(totalparc , parcela))
+
+else:
+    total = 0
+    print('Erro - Opção inválida ')
 
 print('Sua comprar de R${:.2f} vai custar R${:.2f} no final.'.format(preco , total))
+
 #FIM
