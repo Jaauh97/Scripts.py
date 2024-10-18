f7eb04c19f8354fa85f0e1ea2657da0b7921b6a4
João Pedro <jaauhp@gmail.com>
2024-10-18 07:48:53 -0300

    up 01 18.10

---
+++ aulas/Aula 13/aula13.py
@@ -0,0 +1,41 @@
+for c in range(0 , 3):
+    print('OI')   # Estrutura básica de repetição com for
+                  #no range sempre vai entregar um número a menos do quer foi colocado no ()
+print('FIM')
+
+
+for c in range(0, 3+1): # Estrutura básica de repetição com for , para números
+    print(c)
+
+print('FIM')
+
+
+
+for c in range(0 ,9 , 2):
+     print(c) #adiciona um número após a virgula para que a contagem possa pular
+
+
+n = int(input('Digite um número: '))
+for c in range(0, n):
+    print(c)
+
+
+i = int(input('Início: '))
+f = int(input('Final: '))
+p = int(input('Passo: '))
+for c in range(i , f , p):
+    print(c)
+
+
+
+for c in range(0, 3):
+    j = int(input('Digite um número: ')) #loop para pedir 'Informação'
+
+
+s = 0
+for c in range(0, 4):
+    p = int(input('Digite um valor: '))
+    s = s + p
+    print(s)  #Forma básica de somar valores com for
+
+
