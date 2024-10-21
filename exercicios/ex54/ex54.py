2c26ef70ac339566361b098925e5f783238ea4a7
João Pedro <jaauhp@gmail.com>
2024-10-21 15:47:24 -0300

    commit 21.10

--- exercicios/ex54/ex54.py
+++ exercicios/ex54/ex54.py
@@ -1,9 +1,18 @@
-from datetime import date
+from datetime import date #importei a date pra pega a data atual
 
-ano = 0
-atual = date.today().year
+atual = date.today().year #Declarei o ano atual com variavel
+totmaior = 0
+totmenor = 0 #Declarei valor 0 para as variáveis
 
-for c in range(0, 8):
-    data = int(input('{} data de nascimento: ').format(c))
+for pess in range(1, 8):
+    nasc = int(input('Em que ano a {} pessoa nasceu: '.format(pess)))#Coloquei esse pess para poder ir adicionando numero nas perguntas EX: 1 pessoa
+    idade = atual - nasc  #Declarei a idade da pessoa, através de soma básica . O ano atual menos o ano que a pessoa nasceu
+    
+    if idade >= 21: #Se idade maior que 21 então vai adionar mais 1 de valor
+        totmaior += 1
+    else:
+        totmenor += 1 #Se idade menor que 21 então vai adionar mais 1 de valor
+print('Ao todo tivemos {} maiores de idade e tivemos {} menores de idade'.format(totmaior, totmenor))      
+         
     
 
